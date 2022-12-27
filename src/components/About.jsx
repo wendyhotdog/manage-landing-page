193ea86ffebaf928c94e81da860886fbd483b337
wendyhotdog <wendyhotdog101@gmail.com>
2022-12-27 14:34:24 -0400

    Create About component

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about | section-max-w section-margin">
+      <div></div>
+      <div></div>
+    </section>
+  );
+};
+
+export default About;
